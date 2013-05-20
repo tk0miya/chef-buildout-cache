48bdd822768bf2045ff1c3801007bd413568bb7d
Takeshi KOMIYA <i.tkomiya@gmail.com>
2013-05-20 13:56:35 +0900

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['buildout-cache']['user'] = 'app'
+default['buildout-cache']['group'] = 'app'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-buildout-cache'
+maintainer       'Takeshi KOMIYA'
+maintainer_email 'i.tkomiya@gmail.com'
+license          "Apache 2.0"
+description      'Installs/Configures chef-buildout-cache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
